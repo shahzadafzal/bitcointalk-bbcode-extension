da12e9252113194d8fa577eb9562b904324af32c
shahzadafzal <shahzadafzal85@gmail.com>
2025-11-26 22:43:20 +0400

    Updated local with latest from github

--- Extension/background.js
+++ Extension/background.js
@@ -9,11 +9,11 @@ chrome.commands.onCommand.addListener(function (command) {
     });
   }
   
-  if (command === "toggle-italic") {
+  if (command === "toggle-rtl") {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
-        function: executeItalicScript
+        function: executeRTLScript
       });
     });
   }
@@ -36,7 +36,35 @@ chrome.commands.onCommand.addListener(function (command) {
     });
   }
   
+  if (command === "text-glow") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: executeGlow
+      });
+    });
+  }
+  
 });
+
+function executeGlow() {
+	const tagOpen='[glow=red,2,300]';
+	const tagClose='[/glow]';
+	
+   	const textarea = document.querySelector('textarea'); 
+    const selectedText = textarea.value.substring(textarea.selectionStart, textarea.selectionEnd);
+		
+	const newText = `${tagOpen}${selectedText}{$tagClose}`;
+  	
+    const textBeforeSelection = textarea.value.substring(0, textarea.selectionStart);
+    const textAfterSelection = textarea.value.substring(textarea.selectionEnd);
+    const updatedText = textBeforeSelection + newText + textAfterSelection;
+   
+    textarea.value = updatedText;
+
+    textarea.selectionStart = textBeforeSelection.length;
+    textarea.selectionEnd = textarea.selectionStart + newText.length;
+}
 
 function executeScript() {
 	const textarea = document.querySelector('textarea'); 
@@ -50,6 +78,51 @@ function executeScript() {
 
     textarea.selectionStart = textBeforeSelection.length;
     textarea.selectionEnd = textarea.selectionStart + newText.length;
+}
+
+
+function executeRTLScript() {    
+	const textarea = document.querySelector('textarea'); 	
+	const cursorPosition = textarea.selectionStart;
+	const textValue = textarea.value;
+	
+	 if (textarea.getAttribute('dir') === 'rtl') {
+
+		textarea.setAttribute('dir', 'ltr');
+		textarea.value = textValue.replace(/\[right\](.*?)\[\/right\]/g, '$1');
+	} else {
+		textarea.setAttribute('dir', 'rtl');
+		if (textValue.trim() === '') {
+			textarea.value = `[right]\n\n[/right]`;
+			textarea.selectionStart = 8;
+			textarea.selectionEnd = 8;
+		} else {
+			textarea.value = `[right]\n${textValue.trim()}\n[/right]`;
+		}
+	}
+	
+	/*
+	
+	if (textarea.getAttribute('dir') === 'rtl') {
+		textarea.setAttribute('dir', 'ltr');
+		textarea.value = textarea.value.replace(/\[right\](.*?)\[\/right\]/g, '$1');
+	} else {
+		textarea.setAttribute('dir', 'rtl');
+		textarea.value = `[right]${textarea.value}[/right]`;
+	}
+	
+	
+    const selectedText = textarea.value.substring(textarea.selectionStart, textarea.selectionEnd);
+    const newText = `[i]${selectedText}[/i]`;
+    const textBeforeSelection = textarea.value.substring(0, textarea.selectionStart);
+    const textAfterSelection = textarea.value.substring(textarea.selectionEnd);
+    const updatedText = textBeforeSelection + newText + textAfterSelection;
+   
+    textarea.value = updatedText;
+
+    textarea.selectionStart = textBeforeSelection.length;
+    textarea.selectionEnd = textarea.selectionStart + newText.length;
+	*/
 }
 
 function executeItalicScript() {    
